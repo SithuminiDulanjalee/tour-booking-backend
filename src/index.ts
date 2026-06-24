ed159f8f91785721de29c813dcd4a342e63fe267
SithuminiDulanjalee <sithumini2222@gmail.com>
2026-06-24 18:56:00 +0530

    Fix CORS

--- src/index.ts
+++ src/index.ts
@@ -19,14 +19,17 @@ const PORT = process.env.PORT || 5000
 const MONGO_URL = process.env.MONGO_URL || ""
 
 app.use(express.json())
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://tour-booking-frontend-sigma.vercel.app/",
-    /\.vercel\.app$/                   
-  ],
-  credentials: true
-}))
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://tour-booking-frontend-sigma.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true
+  })
+)
+app.use(cors())
 
 app.get("/", (_req, res) => {
   res.json({ message: "VoyaLink API is running" })
